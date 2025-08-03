452babb5bc198f110511b3e74e86b9e52e73f74c
Rilwanu Idris <Rillygulma@gmail.com>
2025-08-03 17:36:23 +0100

    Router Error

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 //import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import router from './routers/Router.jsx'
+import router from './routers/router'
 import AuthProvider from './authentication/AuthProvider.jsx'
 
 
